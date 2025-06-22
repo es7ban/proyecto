64bcf8ee4587b82e26c0a35ff927d76b83cf037b
Esteban <arayawillsesteban@gmail.com>
2025-06-22 18:23:22 -0400

    first commit

---
+++ bacteria.py
@@ -0,0 +1,19 @@
+class Bacteria:
+    def __init__(self):
+        self._id = None
+        self._raza = None
+        self._energia = 0
+        self._resistente = False
+        self._estado = "activa"
+
+    def alimentar(self):
+        print
+
+    def dividirse(self):
+        print
+    
+    def mutar(self):
+        print
+
+    def morir(self):
+        print
