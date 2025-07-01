545cd6bdd8f0c96549abcfd738f486f138c0fe3a
Esteban <arayawillsesteban@gmail.com>
2025-07-01 01:24:06 -0400

    commit 9

--- main.py
+++ main.py
@@ -33,7 +33,24 @@ def visualizar_grilla(ambiente):
         Patch(facecolor=cmap(3 / 5), label='Bacteria resistente'),
         Patch(facecolor=cmap(4 / 5), label='Biofilm (opcional)')
     ]
+    ax.legend(handles=legend_elements, loc='upper right', bbox_to_anchor=(1.4, 1))
+    ax.set_xticks(range(len(matriz[0])))
+    ax.set_yticks(range(len(matriz)))
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    ax.grid(color='gray', linestyle='-', linewidth=0.5)
 
+    for i in range(len(matriz)):
+        for j in range(len(matriz[0])):
+            val = matriz[i][j]
+            if val > 0:
+                ax.text(j, i, str(val), va='center', ha='center', color='white')
+
+    plt.title("Grilla bacteriana (visualización)")
+    plt.tight_layout()
+    plt.show()
+
+#Crear bacteria
 def crear_bacteria():
     b = Bacteria()
     print("\nSeleccione el tipo de bacteria:")
